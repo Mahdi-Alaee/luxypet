3055ca90539ae6bfb50d3d1f49b57e97e1410ebf
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-10-15 21:36:13 +0330

    initial profile breeds page

---
+++ src/app/profile/breeds/page.tsx
@@ -0,0 +1,11 @@
+import UserTabs from "@/components/medium/UserTabs";
+
+export default function Breeds() {
+  return (
+    <main className="max-w-3xl mx-auto pt-12">
+      <UserTabs />
+
+        
+    </main>
+  );
+}
