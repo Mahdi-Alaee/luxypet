6daf7147be8d40a03518534cfb941f00bee8d538
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-11-07 07:23:14 +0330

    handle sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,31 @@
+import { Product } from "@/types/entities";
+
+export default async function sitemap() {
+  const baseUrl = "https://luxypet.ir";
+
+  const res = await (
+    await fetch("http://localhost:3000/api/product", {
+      cache: "no-store",
+    })
+  ).json();
+  let products: { url: string; lastModified: string | undefined }[] = [];
+  if (res.ok) {
+    const productsData = res.data as Product[];
+    products = productsData.map((product) => ({
+      url: `${baseUrl}/product/${product.code}`,
+      lastModified: product.createdAt,
+    }));
+  }
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+    },
+    {
+      url: baseUrl + "/products",
+      lastModified: new Date(),
+    },
+    ...products,
+  ];
+}
