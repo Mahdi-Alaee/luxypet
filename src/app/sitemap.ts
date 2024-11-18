8592310da1cfed558b4942e985407b8f9980e901
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-11-18 21:44:20 +0330

    fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,29 +1,31 @@
 import { Product } from "@/types/entities";
 
 export default async function sitemap() {
-  const baseUrl = "https://luxypet.ir";
-
-  const res = await (
-    await fetch((process?.env?.URL || '')+"/api/product", {
-      cache: "no-store",
-    })
-  ).json();
   let products: { url: string; lastModified: string | undefined }[] = [];
-  if (res.ok) {
-    const productsData = res.data as Product[];
-    products = productsData.map((product) => ({
-      url: `${baseUrl}/product/${product.code}`,
-      lastModified: product.createdAt,
-    }));
+  try {
+    const res = await (
+      await fetch((process?.env?.URL || "") + "/api/product", {
+        cache: "no-store",
+      })
+    ).json();
+    if (res.ok) {
+      const productsData = res.data as Product[];
+      products = productsData.map((product) => ({
+        url: `${process.env.URL}/product/${product.code}`,
+        lastModified: product.createdAt,
+      }));
+    }
+  } catch (err) {
+    console.log(err);
   }
 
   return [
     {
-      url: baseUrl,
+      url: process.env.URL,
       lastModified: new Date(),
     },
     {
-      url: baseUrl + "/products",
+      url: process.env.URL + "/products",
       lastModified: new Date(),
     },
     ...products,
